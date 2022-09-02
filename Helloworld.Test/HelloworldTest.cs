9e24fedb2ab70e0853681f29b7946e5c952c73b2
MadsRoager <94063304+MadsRoager@users.noreply.github.com>
2022-09-02 13:05:01 +0200

    Hello (#1)
    * update
    
    * u
    
    * yes

---
+++ Helloworld.Test/HelloworldTest.cs
@@ -0,0 +1,20 @@
+namespace Helloworld.Test;
+
+public class HelloworldTest
+{
+    [Fact]
+    public void ouputShouldWriteHelloworld()
+    {
+        //Arrange
+        using var writer = new StringWriter();
+        Console.SetOut(writer);
+
+        //Act
+        var program = Assembly.Load(nameof(Helloworld));
+        program.EntryPoint?.Invoke(null, new[] { Array.Empty<string>() });
+
+        //Assert
+        var output = writer.GetStringBuilder().ToString().TrimEnd();
+        output.Should().Be("Hello, World!");
+    }
+}
