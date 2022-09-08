fe3267db452088d70e77245edd3a72b7b3a88d9a
MadsRoager <94063304+MadsRoager@users.noreply.github.com>
2022-09-08 20:38:08 +0200

    Test (#3)
    * e
    
    * Tested and implemented IsLeapYear
    
    * Created user interface for IsLeapYear

---
+++ Helloworld/LeapYear.cs
@@ -0,0 +1,28 @@
+﻿using System;
+namespace Leap {
+public class LeapYear {
+
+    public static void Main(string[] args)
+{
+    Console.WriteLine("Enter a year: ");
+    LeapYear leapYear = new LeapYear();
+    int year = int.Parse(Console.ReadLine());
+    bool isLeapYear = leapYear.IsLeapYear(year);
+    if(isLeapYear) {
+        Console.WriteLine("yay");
+    } else {
+        Console.WriteLine("nay");
+    }
+}
+    public bool IsLeapYear(int year) {
+        if(year % 100 == 0) {
+            if(year % 400 == 0) {
+                return true;
+            }
+            return false;
+        }
+        return year % 4 == 0;
+    }
+}
+}
+
